5320655b95d683be1f05375731a10544827e189e
eigmax <stephen@ieigen.com>
2025-04-24 18:16:53 +0800

    fix: rpc ci

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # safe-box-submiter
+
+1. Install `node` and run the commands below to generate your locking transaction
+
+```
+npm i
+
+export ADDRESS=``
+export PRIVATE_KEY=``
+npm run send
+```
+
+2. Copy the output and push your tx
+
+```
+curl -XPOST https://blockstream.info/testnet/api/tx -H "Content-Type: text/plain" -d "xxxx"
+```
+
--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,34 @@ import ECPairFactory from 'ecpair';
 const network = networks.testnet;
 const ECPair = ECPairFactory(ecc);
 
+const fetch = require('node-fetch'); // or use native fetch in browsers
+const RPC = `https://blockstream.info/testnet/api`
 
-async function submit() {
+async function getUTXOs(address: any) {
+  const url = `${RPC}/address/${address}/utxo`;
+  const response = await fetch(url);
+  const utxos = await response.json();
+
+  console.log(utxos);
+  // Each UTXO contains: txid, vout, value (in sats), status.confirmed, etc.
+  return utxos;
+}
+
+const LOCK_ADDRESS = process.env.ADDRESS || `tb1qfpfy0hhzpax6xkjz9y0ns6hdj36kp04geatuw0`;
+const LOCK_PRIVATE_KEY = process.env.PRIVATE_KEY || ``;
+
+async function getScriptPubKey(txid: any, vout: any) {
+  const res = await fetch(`${RPC}/tx/${txid}`);
+  const tx = await res.json();
+  const output = tx.vout[vout];
+
+  return output.scriptpubkey;
+}
+
+const LOCKED_AMOUNT = 51000;
+const FEE = 200;
+
+async function submit(utxo: any) {
     const serializedStakingData = Buffer.concat([
         Buffer.from("6e756c6c", "hex"), // 4 bytes, endianess not applicable to byte array
         Buffer.from("2d091a1939c276293677886792B77EBa59183c97", "hex") // 20 bytes, endianess not applicable to byte array
@@ -14,46 +40,38 @@ async function submit() {
     const script = btcscript.compile([opcodes.OP_RETURN, serializedStakingData]);
     const psbt = new Psbt({ network });
 
-    let funding_amount = 805200; //sat
-    let locked_amount = 41000;
-    let fee = 200;
-    let changeAmount = funding_amount - locked_amount - fee;
+    let funding_amount = utxo.value; //sat
+    let changeAmount = funding_amount - LOCKED_AMOUNT - FEE;
 
     psbt.addOutputs([
         {
             address: "tb1ql3zk8pngnhlga9xle7hg5zu486u369qttvdwhq",
-            value: locked_amount
+            value: LOCKED_AMOUNT
         },
         {
             script: script,
             value: 0,
         },
         {
-            address: "tb1qfpfy0hhzpax6xkjz9y0ns6hdj36kp04geatuw0", // Your BTC address to receive change
+            address: LOCK_ADDRESS, // Your BTC address to receive change
             value: changeAmount,
         }
     ]);
 
-    let utxo = {
-        txid: "c666bcd234c579bc46ebc229a1cc541dcfb9f91822a2c0b3ce88220f23d5e341",
-        vout: 2,
-        scriptPubKey: "0014485247dee20f4da35a42291f386aed947560bea8",
-    };
-
     // Add Your funding UTXO
+    let scriptPubKey = await getScriptPubKey(utxo.txid, utxo.vout);
     psbt.addInput({
         hash: utxo.txid,
         index: utxo.vout,
         sequence: 0xfffffffd,
         witnessUtxo: {
-            script: Buffer.from(utxo.scriptPubKey, "hex"),
+            script: Buffer.from(scriptPubKey, "hex"),
             value: funding_amount,
         },
     });
 
     // funding account private key
-    let private_key: string = process.env.PRIVATE_KEY || "";
-    const signer = ECPair.fromWIF(private_key, network);
+    const signer = ECPair.fromWIF(LOCK_PRIVATE_KEY, network);
 
     psbt.signInput(0, signer);
 
@@ -72,8 +90,15 @@ async function submit() {
     return tx.toHex();
 }
 
-submit().then((data) => {
-    console.log(
-        `curl -XPOST https://blockstream.info/testnet/api/tx -H "Content-Type: text/plain" -d "${data}"`
-    )
-})
+getUTXOs(LOCK_ADDRESS).then((utxos) => {
+    console.log(`ADDR: ${LOCK_ADDRESS}, KEY: ${LOCK_PRIVATE_KEY}`);
+    if (utxos.length <= 0) {
+        process.exit("No utxo found")
+    }
+    let utxo = utxos.find((item: any) => item.value >= (LOCKED_AMOUNT + FEE))
+    submit(utxo).then((data) => {
+        console.log(
+            `curl -XPOST ${RPC}/tx -H "Content-Type: text/plain" -d "${data}"`
+        )
+    })
+});
